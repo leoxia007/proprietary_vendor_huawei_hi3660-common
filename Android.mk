57ce0f08e5137e8793042abd4aef1856bcb46c61
leoxia007 <38644173+leoxia007@users.noreply.github.com>
2019-03-15 09:19:25 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter berkeley charlotte,$(TARGET_DEVICE)),)
+ifneq ($(filter mha,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := HotwordEnrollmentOKGoogleHI6403
